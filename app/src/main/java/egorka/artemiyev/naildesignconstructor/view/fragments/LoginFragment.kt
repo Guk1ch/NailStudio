1f0360cd4fc9cb60f8bbca7c0ab9d929bb0179a5
Егор Артемьев <batman_batman_1910@mail.ru>
2023-05-21 15:37:05 +0300

    Login Finished

--- app/src/main/java/egorka/artemiyev/naildesignconstructor/view/fragments/LoginFragment.kt
+++ app/src/main/java/egorka/artemiyev/naildesignconstructor/view/fragments/LoginFragment.kt
@@ -5,14 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import egorka.artemiyev.naildesignconstructor.R
+import egorka.artemiyev.naildesignconstructor.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    private val binding: FragmentLoginBinding by lazy { FragmentLoginBinding.inflate(layoutInflater) }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        applyClick()
+    }
+    private fun applyClick(){
+        with(binding){
+            enterButton.setOnClickListener {
+                findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+            }
+            createTxt.setOnClickListener {
+                findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
+            }
+            ForgotPassword.setOnClickListener{
+
+            }
+        }
+
+
     }
 
 }
